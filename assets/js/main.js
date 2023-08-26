2f06ae5963983743f380ce3a9f5b388b7a481229
Juan Camilo Duque Gil <jduque@geovictoria.com>
2023-08-25 22:06:57 -0500

    incio con objetos

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let perro = {
+    // propiedades de un objeto
+    nombre:"Simba",
+    raza:"Shih Tzu",
+    edad:4,
+
+    // métodos de un objeto
+    ladrar(){
+        console.log("El perro a ladrado");
+    }
+};
